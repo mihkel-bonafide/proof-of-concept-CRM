12c9e62e1c1e4119d1af589eaef81c5fdde52dbc
Mihker Gardner <mihkeland@proton.me>
2024-07-01 08:58:11 -0700

    improved documentation for clarity

--- yaml_parsing_operations.py
+++ yaml_parsing_operations.py
@@ -4,13 +4,18 @@ from yaml import load, load_all
 with open("CompanyData.yaml", "r") as meeker:
     safe_company_info = yaml.safe_load(meeker)
 
-print(safe_company_info)
-print()
-print(safe_company_info["Employee"])
+# print(safe_company_info)
+# print()
+# print(safe_company_info["Employee"])  # this is the dictionary array that gets passed to the parsing module
 # print(safe_company_info["Employee"][0])  # prints dictionary of 1st object
 # print() 
 # mihker_salary = safe_company_info["Employee"][0]["Salary"]
 # print(f"Mihker's salary is ${mihker_salary}")
+
+# Okay, that's all fine and great, but can I get address info? 
+yaml_opus = safe_company_info["Employee"]
+print(yaml_opus[0]["Address"]["Street"])
+print(f"{yaml_opus[0]["Address"]["City"]}, {yaml_opus[0]["Address"]["State"]}")
 
 """
 As with our other parsing docs, I simply want to demonstrate how to get at any specified value. I haven't decided how 
